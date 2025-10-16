1ebcc5fc2c95ff1b805ea8ac9d944cc66ad1f2c6
Sara Ibarra <saraiba33@gmail.com>
2025-10-15 20:59:19 -0400

    add cart details page

---
+++ src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
+import { CurrencyPipe, NgForOf, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-details',
+  standalone: true,
+  imports: [NgForOf, CurrencyPipe, NgIf],
+  templateUrl: './cart-details.component.html',
+  styleUrl: './cart-details.component.css',
+})
+export class CartDetailsComponent {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+
+    this.cartService.computeCartTotals();
+  }
+}
--- src/app/components/cart-status/cart-status.component.ts
+++ src/app/components/cart-status/cart-status.component.ts
@@ -3,11 +3,12 @@ import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { CartService } from '../../services/cart.service';
 import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart-status',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './cart-status.component.html',
   styleUrl: './cart-status.component.css',
 })
