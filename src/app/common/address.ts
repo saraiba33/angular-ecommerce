41a260b89207c0b95d6761e27a506d097c51fcd4
Sara Ibarra <saraiba33@gmail.com>
2025-10-29 19:40:42 -0400

    add fields to classes

--- src/app/common/address.ts
+++ src/app/common/address.ts
@@ -1,2 +1,21 @@
 export class Address {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  zipCpde: string;
+
+  constructor(
+    street: string,
+    city: string,
+    state: string,
+    country: string,
+    zipCpde: string
+  ) {
+    this.city = city;
+    this.street = street;
+    this.state = state;
+    this.country = country;
+    this.zipCpde = zipCpde;
+  }
 }
--- src/app/common/purcahse.ts
+++ src/app/common/purcahse.ts
@@ -1,2 +1,26 @@
+import { Address } from './address';
+import { Customer } from './customer';
+import { Order } from './order';
+import { OrderItem } from './order-item';
+
 export class Purcahse {
+  customer: Customer;
+  shippingAddress: Address;
+  billingAddress: Address;
+  order: Order;
+  orderItems: OrderItem[];
+
+  constructor(
+    customer: Customer,
+    shippingAddress: Address,
+    billingAddress: Address,
+    order: Order,
+    orderItems: OrderItem[]
+  ) {
+    this.customer = customer;
+    this.shippingAddress = shippingAddress;
+    this.billingAddress = billingAddress;
+    this.order = order;
+    this.orderItems = orderItems;
+  }
 }
